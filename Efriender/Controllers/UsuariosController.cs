413c959901deedd252569bda6ce9162a91d580f1
erickreis <erick.reis2@gmail.com>
2022-11-09 18:32:56 -0300

    Imagem Perfil

--- Efriender/Controllers/UsuariosController.cs
+++ Efriender/Controllers/UsuariosController.cs
@@ -135,11 +135,6 @@ namespace EFriender.Controllers
 
             usuarios.Nome = User.Identity.Name;
 
-            //if(usuarios.JogoSecond == 0)
-            //{
-            //    usuarios.JogoSecond = null;
-            //}
-
             _context.Attach(usuarios);
             _context.Entry(usuarios).State = EntityState.Added;
             _context.SaveChanges();
