329481d2e9792539e5723dd7e19833fd2d361018
feuvpi <fredvpg@gmail.com>
2022-11-20 01:39:41 -0300

    criando construtores no modelo Combinacao.

--- Efriender/Models/Combinacao.cs
+++ Efriender/Models/Combinacao.cs
@@ -8,6 +8,16 @@ namespace Efriender.Models
     public class Combinacao
     {
 
+
+        #region [ CONSTRUTORES ]
+        public Combinacao(int ID_Usuario1, int ID_Usuario2)
+        {
+            this.Id_Usuario1 = ID_Usuario1;
+            this.Id_Usuario2 = ID_Usuario2;
+        }
+
+        #endregion 
+
         [Key]
         public int Id { get; set; }
         public int Id_Usuario1 { get; set; }
